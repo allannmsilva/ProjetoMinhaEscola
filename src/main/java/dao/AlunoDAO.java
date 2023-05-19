0e249b49696f905c73ca24d2f1f2abd453984128
allan.silva <allan.silva@conexos.inf.br>
2023-05-18 21:23:36 -0300

    Criando listagem de alunos hibernate

--- src/main/java/dao/AlunoDAO.java
+++ src/main/java/dao/AlunoDAO.java
@@ -2,17 +2,53 @@ package dao;
 
 import domain.Aluno;
 import java.util.List;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 public class AlunoDAO {
-    
+
     public static Aluno findById(long id) throws Exception {
         return null;
     }
 
     public static List<Aluno> findList() throws Exception {
-        return null;
+
+        Session sessao = null;
+        EntityTransaction entityTransaction = null;
+        List<Aluno> resultList = null;
+
+        try {
+
+            sessao = ConexaoHibernate.getSessionFactory().openSession();
+            entityTransaction = sessao.getTransaction();
+            entityTransaction.begin();
+
+            CriteriaBuilder criteriaBuilder = sessao.getCriteriaBuilder();
+            CriteriaQuery<Aluno> criteriaQuery = criteriaBuilder.createQuery(Aluno.class);
+            Root<Aluno> root = criteriaQuery.from(Aluno.class);
+            criteriaQuery.select(root);
+            Query<Aluno> query = sessao.createQuery(criteriaQuery);
+            resultList = query.getResultList();
+
+            entityTransaction.commit();
+            sessao.close();
+
+        } catch (HibernateException hex) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                sessao.close();
+            }
+            throw new HibernateException(hex);
+        }
+
+        return resultList;
     }
-    
+
     public static void insert(Aluno body) throws Exception {
     }
 
